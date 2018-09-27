49f9c8ba71e8d5114d69958a479100ff8d7482b1
Minh-Tri Le <trile@snaglobal.net>
2018-09-27 16:48:27 +0700

    Create a Blank Login Fragment

---
+++ app/src/main/java/net/snaglobal/trile/wizeye/LoginFragment.kt
@@ -0,0 +1,36 @@
+package net.snaglobal.trile.wizeye
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * @author trile
+ * @since Sep 27, 2018 at 4:45 PM
+ *
+ * A simple [Fragment] subclass.
+ * Use the [LoginFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment.
+         *
+         * @return A new instance of fragment LoginFragment.
+         */
+        @JvmStatic
+        fun newInstance() = LoginFragment()
+    }
+}
