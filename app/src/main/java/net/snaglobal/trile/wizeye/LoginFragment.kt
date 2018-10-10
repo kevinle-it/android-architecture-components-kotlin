da7dff024306ab307ebd4eb977e82f224cad73cb
Minh-Tri Le <trile@snaglobal.net>
2018-10-10 11:32:12 +0700

    Add Toggle Login Button Enabling function when User Input all Required Fields

--- app/src/main/java/net/snaglobal/trile/wizeye/LoginFragment.kt
+++ app/src/main/java/net/snaglobal/trile/wizeye/LoginFragment.kt
@@ -2,9 +2,14 @@ package net.snaglobal.trile.wizeye
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import kotlinx.android.synthetic.main.fragment_login.*
 
 /**
  * @author trile
@@ -21,6 +26,59 @@ class LoginFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Remove auto-focus from all EditTexts
+        main_container.requestFocus()
+
+        server_address_input.afterTextChanged {
+            toggleLoginButtonEnabling(it, id_input.text.toString(), password_input.text.toString())
+        }
+
+        id_input.afterTextChanged {
+            toggleLoginButtonEnabling(server_address_input.text.toString(), it, password_input.text.toString())
+        }
+
+        password_input.afterTextChanged {
+            toggleLoginButtonEnabling(server_address_input.text.toString(), id_input.text.toString(), it)
+        }
+    }
+    
+    private fun toggleLoginButtonEnabling(serverAddress: String, id: String, password: String) {
+        if (serverAddress.isNotEmpty() && id.isNotEmpty() && password.isNotEmpty()) {
+            login_button.isEnabled = true
+            login_button.background = context?.run {
+                ContextCompat.getDrawable(this, R.drawable.login_button_enabled_background)
+            }
+        } else if (login_button.background.constantState != context?.run {
+                    ContextCompat.getDrawable(this,
+                            R.drawable.login_button_disabled_background)?.constantState
+                }) {
+            login_button.isEnabled = false
+            login_button.background = context?.run {
+                ContextCompat.getDrawable(this, R.drawable.login_button_disabled_background)
+            }
+        } else {
+            login_button.isEnabled = false
+        }
+    }
+
+    private fun EditText.afterTextChanged(afterTextChanged: (String) -> Unit) =
+            this.addTextChangedListener(object : TextWatcher {
+                override fun afterTextChanged(s: Editable?) {
+                    afterTextChanged.invoke(s.toString())
+                }
+
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+                }
+            })
 
 
     companion object {
