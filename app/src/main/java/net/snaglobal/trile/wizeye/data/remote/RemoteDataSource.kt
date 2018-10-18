3a6a18f891c1b57d187ec8d53144082167a7f07b
Minh-Tri Le <trile@snaglobal.net>
2018-10-18 10:34:42 +0700

    Add Remote Data Source

---
+++ app/src/main/java/net/snaglobal/trile/wizeye/data/remote/RemoteDataSource.kt
@@ -0,0 +1,21 @@
+package net.snaglobal.trile.wizeye.data.remote
+
+import net.snaglobal.trile.wizeye.data.remote.login.LoginClient
+import net.snaglobal.trile.wizeye.data.remote.model.LoginResponse
+import net.snaglobal.trile.wizeye.utils.SingletonHolder
+import java.io.IOException
+
+/**
+ * Provides an API for handling all network operations.
+ *
+ * @author lmtri
+ * @since Oct 17, 2018 at 4:49 PM
+ */
+class RemoteDataSource {
+
+    @Throws(IOException::class)
+    fun login(domain: String, username: String, password: String): LoginResponse? =
+            LoginClient.login(domain, username, password)
+
+    companion object : SingletonHolder<RemoteDataSource, Unit>({ RemoteDataSource() })
+}
