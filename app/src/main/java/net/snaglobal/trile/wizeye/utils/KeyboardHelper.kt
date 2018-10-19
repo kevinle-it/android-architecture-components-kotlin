46b79654c46f7f65e260dce2cfc41f3b48c29381
Minh-Tri Le <trile@snaglobal.net>
2018-10-19 11:12:16 +0700

    Add Soft Keyboard Helper

---
+++ app/src/main/java/net/snaglobal/trile/wizeye/utils/KeyboardHelper.kt
@@ -0,0 +1,39 @@
+package net.snaglobal.trile.wizeye.utils
+
+import android.app.Activity
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+
+/**
+ * @author lmtri
+ * @since Oct 15, 2018 at 1:43 PM
+ */
+object KeyboardHelper {
+    @JvmStatic
+    fun showSoftKeyboard(activity: Activity?, editText: EditText) {
+        activity?.run {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm?.run {
+                showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT)
+            }
+        }
+    }
+
+    @JvmStatic
+    fun hideSoftKeyboard(activity: Activity?, clearCurrentFocus: Boolean) {
+        activity?.run {
+            // Check if any view has focus.
+            val view = currentFocus
+            view?.let {
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm?.run {
+                    hideSoftInputFromWindow(view.windowToken, 0)
+                }
+                if (clearCurrentFocus) {
+                    it.clearFocus()
+                }
+            }
+        }
+    }
+}
