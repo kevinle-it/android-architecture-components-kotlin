b23e61de605273514ee15ba181e7c34ce558dc37
Minh-Tri Le <trile@snaglobal.net>
2018-12-03 10:26:09 +0700

    Add Retrofit Client

---
+++ app/src/main/java/net/snaglobal/trile/wizeye/api/RetrofitClient.kt
@@ -0,0 +1,40 @@
+package net.snaglobal.trile.wizeye.api
+
+import android.util.Log
+import net.snaglobal.trile.wizeye.AppExecutors
+import net.snaglobal.trile.wizeye.utils.SingletonHolder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+
+/**
+ * @author lmtri
+ * @since Sep 28, 2018 at 8:37 AM
+ */
+object RetrofitClient : SingletonHolder<Retrofit, String>({ httpUrl: String ->
+    val logger = HttpLoggingInterceptor(HttpLoggingInterceptor.Logger {
+        Log.d("API", it);
+    })
+    logger.level = HttpLoggingInterceptor.Level.HEADERS
+
+    val httpClient = OkHttpClient.Builder()
+            .addInterceptor(logger)
+            .addInterceptor { chain ->
+                val original = chain.request()
+
+                chain.proceed(original)
+            }
+            .connectTimeout(5, TimeUnit.SECONDS)
+            .writeTimeout(8, TimeUnit.SECONDS)
+            .readTimeout(5, TimeUnit.SECONDS)
+            .build()
+
+    Retrofit.Builder()
+            .baseUrl(if (httpUrl.endsWith('/')) httpUrl else "$httpUrl/")
+            .client(httpClient)
+            .callbackExecutor(AppExecutors.getInstance(Unit).networkIO())
+            .addConverterFactory(MoshiConverterFactory.create().withNullSerialization())
+            .build()
+})
